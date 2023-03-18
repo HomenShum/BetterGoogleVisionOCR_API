5d842cd0e4e35b8ac48ea24380c243c3e3db880c
Homen Shum <77596827+HomenShum@users.noreply.github.com>
2023-03-17 22:21:38 -0700

    Update googleVisionBatchExtract.py

--- googleVisionBatchExtract.py
+++ googleVisionBatchExtract.py
@@ -1,3 +1,12 @@
+""" INSTRUCTIONS
+2a. create a project on google cloud
+2b. get google application credentials and store into your files, this/the project helps google to charge your account for your usage
+2c. store pdf file that needs to be text extracted onto your google cloud storage folder (also called the bucket); for example below, I have my bucket named pdf_test_hs. This/gcs helps google to utilize their technology on your files.
+2d. get the "gsutil URI"; click on the pdf file in your gcs, the hyperlink will take you to the pdf details page, within the detail table you will find the "gsutil URI"
+2e. insert your gsutil URI into the gcs_list, if you replace the existing URI and only have one gsutil URI, then put gcs_list_selected = gcs_list[0]
+2f. *** batch_size = 10; may run into issues if your 10 pages of text exceeds 10mb size, so decrease the size if necessary, but I don't think many pdf pages will have more than 10mb of pure texts...
+RUN PYTHON FILE => GO TO STEP 3
+"""
 #Skip if json batch folder already created in bucket
 #1 pdf page is a unit, 1000 unit free per month
 #IN FUTURE: SORT THROUGH LIST OF PDF FILES IN GCS BUCKET THEN PROCESS SELECTED PDF FILES
